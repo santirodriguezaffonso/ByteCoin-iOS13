e954fc4669c53fd7a821b26514bc8ca7992837df
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-03-29 14:55:46 -0300

    cleaning code

--- ByteCoin/Controller/CoinViewController.swift
+++ ByteCoin/Controller/CoinViewController.swift
@@ -1,21 +1,9 @@
 
 import UIKit
 
-class CoinViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class CoinViewController: UIViewController {
     
     var coinManager = CoinManager()
-    
-    // This code lines will determinate how many columns should have our PickerView
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // This code lines will determinate how many rows should have our PickerView
-    // In this case we're using currencyArray directly
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return coinManager.currencyArray.count
-    }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,17 +17,6 @@ class CoinViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
-    
-    // This method will ask to delegate for a "row title", in this case "func pickerView(: numberOfRowInComponent..."
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return coinManager.currencyArray[row]
-        
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedCurrency = coinManager.currencyArray[row]
-        coinManager.getCoinPrice(for: selectedCurrency)
-    }
 }
 
 //MARK: – CoinManagerDelegate –
@@ -56,4 +33,36 @@ extension CoinViewController: CoinManagerDelegate {
         print(error)
     }
     
+}
+
+//MARK: – UIPickerView DataSource & Delegate –
+
+extension CoinViewController: UIPickerViewDataSource {
+   
+    // This code lines will determinate how many columns should have our PickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // This code lines will determinate how many rows should have our PickerView
+    // In this case we're using currencyArray directly
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+
+extension CoinViewController: UIPickerViewDelegate {
+    
+    // This method will ask to delegate for a "row title", in this case "func pickerView(: numberOfRowInComponent..."
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+        
+    }
+    // This method will give you and output from UIPickerView "(: didSelectRow" to use what was selected
+    // in this case we call a fucn with an input, dont miss that
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 }
