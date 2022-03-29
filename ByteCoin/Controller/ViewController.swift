6627597ef81e348ee0d016e8d3f594c33db975d4
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-03-29 11:56:26 -0300

    func pickerView(: didSelectedRow added, URLSession added

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     // This code lines will determinate how many rows should have our PickerView
+    // In this case we're using currencyArray directly
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
@@ -30,6 +31,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     // This method will ask to delegate for a "row title", in this case "func pickerView(: numberOfRowInComponent..."
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+
     }
 }
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,6 +7,32 @@ struct CoinManager {
     let apiKey = "405D3920-A6C4-491D-B1D7-02821C7CFEE9"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+//                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString)
+//                if let safeData = data {
+//                    if let weather = self.parseJSON(safeData) {
+//                        delegate?.didUpdateWeather(self, weather: weather)
+//                    }
+//                }
+            }
+            task.resume()
+        }
+    }
+    
     
 }
